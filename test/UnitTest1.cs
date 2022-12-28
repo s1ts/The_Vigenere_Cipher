d1dd8302699b4c43d59882c9956c95fa5968f139
s1ts <107190958+s1ts@users.noreply.github.com>
2022-12-28 14:59:50 +0700

    Add files via upload

---
+++ test/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObjectOrientedPractics.Services;
+using protect_inf_LR1;
+using System;
+using System.Collections.Generic;
+using protect_inf_LR1;
+
+namespace test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAuth()
+        {
+            List<User> list = new List<User>();
+            list = Serializer.LoadFromFile("users.txt");
+            string login = "admin";
+            string password = "admin";
+            bool good = false;
+            foreach (var usr in list)
+            {
+                if (usr.login == login && usr.password == password)
+                {
+                    good = true;
+                }
+            }
+            Assert.AreEqual(good, true);
+        }
+        [TestMethod]
+        public void TestRegister()
+        {
+            User user = new User();
+            List<User> list = new List<User>();
+            list = Serializer.LoadFromFile("users.txt");
+            string login = "admin1";
+            string password = "admin1";
+            user.login = login;
+            user.password = password;
+            list.Add(user);
+            bool good = false;
+            foreach (var usr in list)
+            {
+                if (usr.login == login && usr.password == password)
+                {
+                    good = true;
+                }
+            }
+            Assert.AreEqual(good, true);
+        }
+        [TestMethod]
+        public void TestAuthFalse()
+        {
+            List<User> list = new List<User>();
+            list = Serializer.LoadFromFile("users.txt");
+            string login = "admin55";
+            string password = "admin55";
+            bool good = false;
+            foreach (var usr in list)
+            {
+                if (usr.login == login && usr.password == password)
+                {
+                    good = true;
+                }
+            }
+            Assert.AreEqual(good, false);
+        }
+    }
+}
